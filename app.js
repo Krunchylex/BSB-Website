42777a5726851b58be496c4c2838288d76dd7780
Alexander <10575796+Krunchylex@users.noreply.github.com>
2019-09-01 20:08:53 -0400

    partially switch database from mongo to mysql and remove old models and dependencies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express       = require("express"),
     app           = express(),
     bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose"),
+    mysql         = require("mysql"),
     passport      = require("passport"),
     localStrategy = require("passport-local"),
     User          = require("./models/user"),
@@ -23,11 +23,18 @@ app.use(require("express-session")({
 })) ;
 app.use(passport.initialize()) ;
 app.use(passport.session()) ;
-passport.use(new localStrategy(User.authenticate())) ;
+//passport.use(User.crea ;
 passport.serializeUser(User.serializeUser()) ;
 passport.deserializeUser(User.deserializeUser()) ;
 
-// routes
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user ;
+    res.locals.error = req.flash("error") ;
+    res.locals.success = req.flash("success") ;
+    next() ;
+})
+
+// page routes
 
 app.get("/", function(req, res){
     res.render("index") ;
@@ -47,8 +54,36 @@ app.get("/register", function(req, res){
     res.render("register") ;
 }) ;
 
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username}, {email: req.body.email}) ;
+    User.register(newUser, req.body.password, function(err, user){
+        if (err){
+            console.log(err) ;
+            req.flash("Unable to register", err.message) ;
+            return res.redirect("register") ;
+        }
+        passport.authenticate("local")(function(req, res){
+            res.redirect("back") ;
+        }) ;
+    }) ;
+}) ;
+
 app.get("/login", function(req, res){
     res.render("login") ;
+}) ;
+
+app.post("/login", function(req, res){
+    passport.authenticate("local", {
+        successRedirect: "back",
+        failureRedirect: "/login",
+        failureFlash: true
+    }) ;
+}) ;
+
+app.get("/logout", function(req, res){
+    req.logout() ;
+    req.flash("success", "Logged out") ;
+    res.redirect("/") ;
 }) ;
 
 // listening port
